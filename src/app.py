2e1faa60467cdffb0ce23ae032a7704a91d53857
sepeca15 <sepeca15@gmail.com>
2022-08-05 23:25:52 +0000

    api flask en camino

--- src/app.py
+++ src/app.py
@@ -1,4 +1,10 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask
 
-# your code here
+app = Flask(__name__)
+
+@app.route("/")
+def hello_flask():
+    return "Hello Flask"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5000)
