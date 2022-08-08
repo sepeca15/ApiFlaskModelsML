d312da5f75069d3bf1928f2242e144a091f18ea9
sepeca15 <sepeca15@gmail.com>
2022-08-08 21:56:03 +0000

    test.py yes

--- src/test.py
+++ src/test.py
@@ -2,14 +2,14 @@ import pickle
 import pandas as pd 
 import numpy as np 
 
-country = "Other"
-variety = "Other"
-aroma = 7.42
-afterteaste = 7.33
-acidity = 7.42
-body = 7.25
-balance = 7.33
-moisture = 0.0
+country = "Colombia"
+variety = "Caturra"
+aroma = 7.83
+afterteaste = 7.67
+acidity = 7.33
+body = 7.67
+balance = 7.67
+moisture = 0.11
 
 cols = ['country_of_origin', 'variety', 'aroma', 'aftertaste', 'acidity', 'body', 'balance', 'moisture']
 data = [country, variety, aroma, afterteaste, acidity, body, balance, moisture]
